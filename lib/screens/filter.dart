a7d7fddf48148caa03ade16f92e63f0d00e9ea1d
Eliana Ferreira <ebferreiran@gmail.com>
2023-07-03 23:26:41 -0400

    using filtersProvider instead of local states for Filters Screen

--- lib/screens/filter.dart
+++ lib/screens/filter.dart
@@ -4,82 +4,55 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_recipes/providers/filters_provider.dart';
 import 'package:meals_recipes/widgets/filter_item.dart';
 
-class FiltersScreen extends ConsumerStatefulWidget {
+class FiltersScreen extends ConsumerWidget {
   const FiltersScreen({super.key});
 
   @override
-  ConsumerState<FiltersScreen> createState() {
-    return _FiltersState();
-  }
-}
-
-class _FiltersState extends ConsumerState<FiltersScreen> {
-  bool _glutenFreeFilterSet = false;
-  bool _lactoseFreenFilterSet = false;
-  bool _vegetarianFilterSet = false;
-  bool _veganFilterSet = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    final filters = ref.read(filtersProvider);
-    _glutenFreeFilterSet = filters[Filter.glutenFree]!;
-    _lactoseFreenFilterSet = filters[Filter.lactoseFree]!;
-    _vegetarianFilterSet = filters[Filter.vegetarian]!;
-    _veganFilterSet = filters[Filter.vegan]!;
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final activeFilters = ref.watch(filtersProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Your Filters')),
-      body: WillPopScope(
-        onWillPop: () async {
-          ref.watch(filtersProvider.notifier).setFilters({
-            Filter.glutenFree: _glutenFreeFilterSet,
-            Filter.lactoseFree: _lactoseFreenFilterSet,
-            Filter.vegetarian: _vegetarianFilterSet,
-            Filter.vegan: _veganFilterSet,
-          });
-          return true;
-        },
-        child: Column(
-          children: [
-            FilterItem(
-              title: 'Gluten-free',
-              subtitle: 'Only inlcude gluten-free meals.',
-              filterValue: _glutenFreeFilterSet,
-              onCheckedChange: (isChecked) => setState(() {
-                _glutenFreeFilterSet = isChecked;
-              }),
-            ),
-            FilterItem(
+      body: Column(
+        children: [
+          FilterItem(
+            title: 'Gluten-free',
+            subtitle: 'Only inlcude gluten-free meals.',
+            filterValue: activeFilters[Filter.glutenFree]!,
+            onCheckedChange: (isChecked) {
+              ref
+                  .read(filtersProvider.notifier)
+                  .setFilter(Filter.glutenFree, isChecked);
+            },
+          ),
+          FilterItem(
               title: 'Lactose-free',
               subtitle: 'Only inlcude lactose-free meals.',
-              filterValue: _lactoseFreenFilterSet,
-              onCheckedChange: (isChecked) => setState(() {
-                _lactoseFreenFilterSet = isChecked;
+              filterValue: activeFilters[Filter.lactoseFree]!,
+              onCheckedChange: (isChecked) {
+                ref
+                    .read(filtersProvider.notifier)
+                    .setFilter(Filter.lactoseFree, isChecked);
               }),
-            ),
-            FilterItem(
-              title: 'Vegetarian',
-              subtitle: 'Only inlcude vegetarian meals.',
-              filterValue: _vegetarianFilterSet,
-              onCheckedChange: (isChecked) => setState(() {
-                _vegetarianFilterSet = isChecked;
-              }),
-            ),
-            FilterItem(
+          FilterItem(
+            title: 'Vegetarian',
+            subtitle: 'Only inlcude vegetarian meals.',
+            filterValue: activeFilters[Filter.vegetarian]!,
+            onCheckedChange: (isChecked) {
+              ref
+                  .read(filtersProvider.notifier)
+                  .setFilter(Filter.vegetarian, isChecked);
+            },
+          ),
+          FilterItem(
               title: 'Vegan ',
               subtitle: 'Only inlcude vegan meals.',
-              filterValue: _veganFilterSet,
-              onCheckedChange: (isChecked) => setState(() {
-                _veganFilterSet = isChecked;
+              filterValue: activeFilters[Filter.vegan]!,
+              onCheckedChange: (isChecked) {
+                ref
+                    .read(filtersProvider.notifier)
+                    .setFilter(Filter.vegan, isChecked);
               }),
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
